1c6c2d52388a453216f0fbbafe3eb830df4495d5
Prajakta Bujare <bujare.prajakta17@gmail.com>
2023-01-17 23:38:47 +0530

    first commit

---
+++ basic/quote-generator/js/script.js
@@ -0,0 +1,4 @@
+console.log(
+  'https://forum.freecodecamp.org/t/free-api-inspirational-quotes-json-with-code-examples/311373'
+);
+console.log('https://rapidapi.com/yoavshalev/api/quote-of-the-day/');
