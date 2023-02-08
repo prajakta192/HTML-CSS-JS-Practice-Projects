0abcd59ac376d31dad198946be3c2c4e8fd4f110
Prajakta Bujare <73356971+prajakta192@users.noreply.github.com>
2023-02-08 12:38:58 +0000

    added Hour format(12hr, 24hr)

--- basic/Digital-clock/Script/script.js
+++ basic/Digital-clock/Script/script.js
@@ -1,42 +1,55 @@
-if(document.readyState === 'loading'){
+const toggleBtn = document.querySelector('.hrFormat');
+toggleBtn.addEventListener('change', getHourFormat)
 
-    document.addEventListener('DOMContentLoaded', ready)
+//changing Hour Format
+function getHourFormat(){
+    const btnVal = toggleBtn.value;
+    return btnVal
+
 }
-else{
-    ready()
-}
+//const hrFormat = getHourFormat()
+//console.log(hrFormat)
+ 
+function updateClock(){
 
+    const hrFormat = getHourFormat();
 
+    const hours = document.querySelector('.hours');
+    const minutes = document.querySelector('.minutes');
+    const seconds = document.querySelector('.seconds');
+    const Mseconds = document.querySelector('.Mseconds');
+    const ampm = document.querySelector('.ampm');
+    
+ 
+    const date = new Date();
+    const h = date.getHours();
+        const m = date.getMinutes();
+        const s = date.getSeconds();
+        const ms = date.getMilliseconds();
 
-
-function ready(){
-   // console.log('ready')
-  
-   //setting hours, minutes, seconds 
-   const hours = document.querySelector('.hours');
-   const minutes = document.querySelector('.minutes');
-   const seconds = document.querySelector('.seconds');
-   const Mseconds = document.querySelector('.Mseconds');
-   const ampm = document.querySelector('.ampm')
-
-   const date = new Date();
-   const h = date.getHours();
-   const m = date.getMinutes();
-   const s = date.getSeconds();
-   const ms = date.getMilliseconds();
-   hours.textContent = h < 12 ? `0${h}` : `0${h -12}`;
-   minutes.textContent = m < 10 ? `0${m}` : m;
-   seconds.textContent = s < 10 ? `0${s}` : s;
-   //Mseconds.textContent = ms < 10 ? `00${ms}` : ms
+    //console.log(hrFormat)
+    if(hrFormat === '12Hr'){
+        hours.textContent = h < 12 ? `0${h}` : `0${h -12}`;
+        minutes.textContent = m < 10 ? `0${m}` : m;
+        seconds.textContent = s < 10 ? `0${s}` : s;
+        //Mseconds.textContent = ms < 10 ? `00${ms}` : ms
+        
+        ampm.textContent = h >= 12 ? 'PM' : 'AM';
+     
+    }else{
+        hours.textContent = h < 12 ? `0${h}` : h;
+        minutes.textContent = m < 10 ? `0${m}` : m;
+        seconds.textContent = s < 10 ? `0${s}` : s;
+        //Mseconds.textContent = ms < 10 ? `00${ms}` : ms
+        ampm.textContent = h >= 12 ? 'PM' : 'AM';
+        
+    }
    
-   ampm.textContent = h >= 12 ? 'PM' : 'AM';
-
-
 }
 
 setInterval(() => {
-   ready()
+    updateClock()
 },1000) 
    
-   
+updateClock()
    
